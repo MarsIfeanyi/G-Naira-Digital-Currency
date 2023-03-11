3fa424e516401e7cd3c78f8cf306d0cfb1434d2c
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 21:26:15 +0100

    Update style

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="text-blue-600 font-normal items-start md:font-medium text-4xl mt-10 ">
-      #G-Naira
+    <div className="font-bold text-blue-600  items-start md:font-medium text-4xl mt-10 ">
+      <span className="italic">#G-Naira</span>
     </div>
   );
 };
