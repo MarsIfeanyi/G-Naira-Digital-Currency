81a40a140e3163434e931228877ca1dbe0baebad
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 20:29:25 +0100

    Initial Commit

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+import GiveMeFeedBack from "./components/GiveMeFeedBack";
+
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+
+import Footer from "./components/Footer";
+import MintTokenPage from "./pages/MintTokenPage";
+import BurnTokenPage from "./pages/BurnTokenPage";
+import BlacklistUserPage from "./pages/BlacklistUserPage";
+
+const App = () => {
+  return (
+    <div className=" justify-between  max-w-full p-2 mx-6 bg-white ">
+      <MainLayout />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mint-token" element={<MintTokenPage />} />
+        <Route path="/burn-token" element={<BurnTokenPage />} />
+        <Route path="/blacklist" element={<BlacklistUserPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
