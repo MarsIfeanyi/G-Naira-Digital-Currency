ce46287a729cd3074c8fe35a138bc98aabaeda09
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 20:31:01 +0100

    Initial Commit

---
+++ frontEnd/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="
+    mx-auto text-center  justify-center mt-36 font-normal mb-10 text-blue-600"
+    >
+      <p> Built with ❤️ by Mars &copy; Learnable22 </p>
+    </div>
+  );
+};
+
+export default Footer;
