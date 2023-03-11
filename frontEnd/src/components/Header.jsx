ad3e3376085bd5b8945562c715c691f03b9cedaa
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 20:31:38 +0100

    Initial Commit

---
+++ frontEnd/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-blue-600 font-normal items-start md:font-medium text-4xl mt-10 ">
+      #G-Naira
+    </div>
+  );
+};
+
+export default Header;
