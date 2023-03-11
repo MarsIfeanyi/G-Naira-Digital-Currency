e58bb3b4e7f58bd63f336477f8ed720663c7260a
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 20:31:50 +0100

    Initial Commit

---
+++ frontEnd/src/components/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+//import UserAuth from "./UserAuth";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <div className="flex flex-col space-y-6 space-x-0 md:flex-row  md:justify-between 10  text-blue-600 ml-10  ">
+        <Header />
+        {/* 
+        <UserAuth /> */}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
