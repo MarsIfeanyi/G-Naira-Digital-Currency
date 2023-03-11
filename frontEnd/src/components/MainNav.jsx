d7ef65329519148e09730d2ce19f7d45fd74978e
MarsIfeanyi <marschalice@gmail.com>
2023-03-11 20:31:58 +0100

    Initial Commit

---
+++ frontEnd/src/components/MainNav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const MainNav = () => {
+  return (
+    <div>
+      <div className=" items-center justify-center mx-auto flex flex-col md:flex-row space-x-0 md:space-x-10 space-y-10 md:space-y-0 mt-24 ">
+        <Link to="/mint-token">
+          <button className="bg-blue-600 rounded-xl text-white  py-2 px-3 text-xl ">
+            Mint Token
+          </button>
+        </Link>
+
+        <Link to="/burn-token">
+          <button className="bg-blue-600 rounded-xl text-white  py-2 px-3 text-xl ">
+            Burn Token
+          </button>
+        </Link>
+
+        <Link to="/blacklist">
+          <button className="bg-blue-600 rounded-xl text-white  py-2 px-3 text-xl">
+            BlackList User
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MainNav;
