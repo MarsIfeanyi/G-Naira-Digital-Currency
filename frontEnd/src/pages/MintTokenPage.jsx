2d57c0e9b3fb47794d347827d9a7b79f77bde209
MarsIfeanyi <marschalice@gmail.com>
2023-03-13 03:32:57 +0100

    Update component

--- frontEnd/src/pages/MintTokenPage.jsx
+++ frontEnd/src/pages/MintTokenPage.jsx
@@ -1,12 +1,15 @@
 import { ethers } from "ethers";
 import { useState } from "react";
 import Web3Modal from "web3modal";
+import Balances from "../components/Balances";
 
 import { contractABI, contractAddress } from "../constants/config";
 
 const MintTokenPage = () => {
-  const [mintToken, setMintT] = useState("");
-  const [transactionHash, setTransactionHash] = useState();
+  const [recipient, setRecipient] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const [transactionHash, setTransactionHash] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,58 +22,69 @@ const MintTokenPage = () => {
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    //const transaction = await contract.sendFeedBack(feedback);
+    if (recipient && amount) {
+      const mintTokenTransaction = await contract.mintTokenCurrency(
+        recipient,
+        amount
+      );
 
-    setTransactionHash(`
-            Mining... ${transaction.hash}
-            
-            `);
-    await transaction.wait();
-    setTransactionHash(
-      `
-            Mined... ${transaction.hash}`
-    );
+      setTransactionHash(`Mining... ${mintTokenTransaction.hash}`);
+      await mintTokenTransaction.wait();
+      setTransactionHash(`Mined... ${mintTokenTransaction.hash}`);
 
-    setFeedback("");
+      setRecipient("");
+      setAmount("");
+    }
   };
 
   return (
-    <div
-      className="mt-20 
-    "
-    >
-      <h1
-        className="justify-center text-3xl mx-auto items-center text-center text-blue-600 
-      "
-      >
+    <div className="mt-10">
+      <h1 className="justify-center text-3xl mx-auto items-center text-center text-blue-600">
         Mint New Token Currency
       </h1>
 
       <form onSubmit={handleSubmit}>
-        <div className="flex flex-col space-y-6  space-x-0  md:space-x-4  mx-auto items-center justify-center mt-14">
+        <div className="flex flex-col space-y-6 md:space-x-4 mx-auto items-center justify-center mt-14">
           {/* Individual Container */}
-          <div className="space-y-2 md:space-y-0 flex-col md:flex-row  ">
-            <label htmlFor="feedback" className="text-blue-600 mr-2">
-              Recipient - Amount
+          <div className="space-y-2 flex flex-col">
+            <label htmlFor="recipient" className="text-blue-600 mr-2">
+              Recipient
             </label>
 
             <input
               type="text"
-              value={feedback}
-              placeholder="Enter Address(account) and Amount"
+              value={recipient}
+              placeholder="Enter Address (account)"
               required
-              onChange={(e) => setFeedback(e.target.value)}
+              onChange={(e) => setRecipient(e.target.value)}
               className="rounded-lg p-2 border border-blue-600 w-72"
             />
+
+            <label htmlFor="amount" className="text-blue-600 mr-2">
+              Amount
+            </label>
+
+            <input
+              type="number"
+              value={amount}
+              placeholder="Enter Amount"
+              required
+              onChange={(e) => setAmount(e.target.value)}
+              className="rounded-lg p-2 border border-blue-600 w-72"
+            />
+
             <button
               type="submit"
-              className="bg-blue-600 rounded-xl py-2 px-4 text-white ml-2   "
+              className="bg-blue-600 rounded-xl py-2 px-4 text-white ml-2"
             >
               Mint Token
             </button>
           </div>
         </div>
       </form>
+      <h3 className="text-center justify-center mt-8">{transactionHash}</h3>
+
+      <Balances />
     </div>
   );
 };
