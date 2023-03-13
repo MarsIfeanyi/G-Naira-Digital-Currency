1c62f958c5982b9ed248ae9203172ee52fcd0ff0
MarsIfeanyi <marschalice@gmail.com>
2023-03-13 03:27:36 +0100

    Add Link to Transfer Page

--- frontEnd/src/components/MainNav.jsx
+++ frontEnd/src/components/MainNav.jsx
@@ -22,6 +22,12 @@ const MainNav = () => {
             BlackList User
           </button>
         </Link>
+
+        <Link to="/transfer">
+          <button className="bg-green-800 rounded-xl text-white  py-2 px-3 text-xl">
+            Transfer
+          </button>
+        </Link>
       </div>
     </div>
   );
