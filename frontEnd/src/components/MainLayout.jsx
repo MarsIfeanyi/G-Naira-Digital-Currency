cd810421211923228dc57840412bcd74430fcc9d
MarsIfeanyi <marschalice@gmail.com>
2023-03-13 03:26:42 +0100

    Uncomment  UserAuth Component

--- frontEnd/src/components/MainLayout.jsx
+++ frontEnd/src/components/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
-//import UserAuth from "./UserAuth";
+import UserAuth from "./UserAuth";
 
 const MainLayout = () => {
   return (
     <div>
       <div className="flex flex-col space-y-6 space-x-0 md:flex-row  md:justify-between 10  text-blue-600 ml-10  ">
         <Header />
-        {/* 
-        <UserAuth /> */}
+
+        <UserAuth />
       </div>
     </div>
   );
